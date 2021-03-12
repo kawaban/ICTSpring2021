2d0957d02692380039b873efc7f497898451cc46
kawaban <64012749+kawaban@users.noreply.github.com>
2021-03-12 18:10:53 +0300

    Add files via upload

---
+++ Week2/Example1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F4();
+        }
+
+        private static void PrintFolderInfo(string path, string prefix = "")
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            try
+            {
+                foreach (FileSystemInfo fi in dir.GetFileSystemInfos())
+                {
+                    Console.WriteLine(prefix + " " + fi.Name);
+                    if (fi.GetType() == typeof(DirectoryInfo))
+                    {
+                        PrintFolderInfo(fi.FullName, prefix + "--");
+                    }
+                }
+            } 
+            catch (Exception) { }
+            
+        }
+
+        private static void F4()
+        {
+            PrintFolderInfo(@"C:\Users\админ\Documents");
+        }
+        private static void getFileSystemsNames()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\админ\Documents\National Instruments\Circuit Design Suite 14.0");
+
+            foreach(FileSystemInfo fi in dir.GetFileSystemInfos())
+            {
+                Console.WriteLine(fi.Name);
+            }
+        }
+
+        private static void getDirectoriesAndFiles()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\админ\Documents\National Instruments\Circuit Design Suite 14.0");
+
+            foreach(DirectoryInfo d in dir.GetDirectories())
+            {
+                Console.WriteLine(d.Name);
+            }
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                Console.WriteLine(f.Name);
+            }
+        }
+
+        private static void getDirectoryName()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\админ\Documents\National Instruments\");
+
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                Console.WriteLine(d.FullName);
+            }
+        }
+
+        private static void getDisksName()
+        {
+            foreach (string l in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine(l);
+            }
+        }
+
+        
+    }
+}
---
+++ Week2/Example3/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Example2
+{
+
+    class Layer
+    {
+        public DirectoryInfo dir
+        {
+            get;
+            set;
+        }
+        public int pos
+        {
+            get;
+            set;
+        }
+        public List<FileSystemInfo> content
+        {
+            get;
+            set;
+        }
+
+        public Layer(DirectoryInfo dir, int pos)
+        {
+            this.dir = dir;
+            this.pos = pos;
+            this.content = new List<FileSystemInfo>();
+
+
+
+            content.AddRange(this.dir.GetDirectories());
+            content.AddRange(this.dir.GetFiles());
+        }
+
+        public void PrintInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            int cnt = 0;
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                DateTime creationTime = d.CreationTime;
+
+                long size = 0;
+                foreach (FileInfo fi in d.GetFiles())
+                {
+
+                    size += fi.Length;
+                }
+                Console.WriteLine(d.Name + "     |  " + size + " bytes  |  Created: " + creationTime);
+
+                cnt++;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (FileInfo f in dir.GetFiles())
+            {
+
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                DateTime creationTime = f.CreationTime;
+
+                Console.WriteLine(f.Name + "     |  " + f.Length + " bytes  |  Created: " + creationTime);
+                cnt++;
+            }
+        }
+
+        public void OpenFile(string path, string extension, bool isFile)
+        {
+   
+           
+            if (extension == ".txt")
+            {
+                StreamReader sr = new StreamReader(path);
+                Console.WriteLine(sr.ReadToEnd());
+                    
+                //string readFile = File.ReadAllText(path);
+                //Console.WriteLine(readFile);
+            }
+            else Console.WriteLine("Sorry, can not open file");
+               
+
+            
+            
+          
+        }
+
+        public FileSystemInfo GetCurrentObject()
+        {
+            return content[pos];
+        }
+
+        public void SetNewPosition(int d)
+        {
+            if (d > 0)
+            {
+                pos++;
+            }
+            else
+            {
+                pos--;
+            }
+            if (pos >= content.Count)
+            {
+                pos = 0;
+            }
+            else if (pos < 0)
+            {
+                pos = content.Count - 1;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F3();
+        }
+
+        private static void F3()
+        {
+
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(new Layer(new DirectoryInfo(@"C:\Users\админ\Documents\rktko"), 0));
+
+            bool escape = false;
+            bool isFile = false;
+
+            while (!escape)
+            {
+                Console.Clear();
+
+                history.Peek().PrintInfo();
+
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (history.Peek().GetCurrentObject().GetType() == typeof(DirectoryInfo))
+                        {
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as DirectoryInfo, 0));
+                        }
+                        else if (history.Peek().GetCurrentObject().GetType() == typeof(FileInfo))
+                        {
+                            isFile = true;
+                            history.Peek().OpenFile(history.Peek().GetCurrentObject().FullName, history.Peek().GetCurrentObject().Extension, isFile);
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SetNewPosition(-1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SetNewPosition(1);
+                        break;
+                    case ConsoleKey.Escape:
+                        history.Pop();
+                        break;
+                }
+            }
+        }
+
+        private static void F2()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                if (consoleKeyInfo.Key == ConsoleKey.Escape) break;
+                Console.WriteLine(consoleKeyInfo.KeyChar);
+            }
+        }
+
+        private static void F1()
+        {
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+            Console.WriteLine(consoleKeyInfo);
+        }
+    }
+}
